8ce63900a287d18e17690528e1a953d87a5ce19d
mucahitcakmak <mucoox18@gmail.com>
2024-08-25 18:40:25 +0300

    SPA alt başlıklara yönlendirme tamamlandı. Yönlendirme yapılırken bir üst başlık tabbarda işaretleniyor.

--- static/js/SPA.js
+++ static/js/SPA.js
@@ -1,65 +1,140 @@
 // URL yapılarını ve içeriklerini yönetmek için bir nesne tanımlayalım
 const routes = {
     "/home": { title: "Home", contentPath: "../templates/pages/home.html" },
-
-    // alt dalları denemek için test ediliyor. şuanda html dosyası bulunmuyor.
-    "/home/play-against-ai": { title: "Play Against AI", contentPath: "../templates/pages/play-against-ai.html" },
     "/leaderboard": { title: "Leaderboard", contentPath: "../templates/pages/leaderboard.html" },
+    
     "/statistics": { title: "Statistics", contentPath: "../templates/pages/statistics.html" },
+    "/statistics/matchs-history": { title: "Matchs-History", contentPath: "../templates/pages/matchs-history.html" },
+    
     "/friends": { title: "Friends", contentPath: "../templates/pages/friends.html" },
     "/profile": { title: "Profile", contentPath: "../templates/pages/profile.html" },
 };
 
+document.addEventListener('DOMContentLoaded', () => {
+    const indicator = document.querySelector('.nav-indicator');
+    const contentElement = document.getElementById('content');
 
-window.onload = function () {
+    function handleIndicator(el) {
+        if (el.classList.contains('nav-item')) {
+            const items = document.querySelectorAll('.nav-item'); // Yeni nav-item'ları da dahil et
+            items.forEach(item => {
+                item.classList.remove('is-active');
+                item.style.color = ''; // Renk sıfırlama
+            });
+
+            indicator.style.width = `${el.offsetWidth}px`;
+            indicator.style.left = `${el.offsetLeft}px`;
+            indicator.style.backgroundColor = el.getAttribute('active-color');
+
+            el.classList.add('is-active');
+            el.style.color = el.getAttribute('active-color');
+        }
+    }
+
     function updatePageContent(html, pageTitle, urlPath) {
-        const contentElement = document.getElementById("content");
+        // Fade-out başlangıcı
+        contentElement.classList.remove('show');
         
-        // Önce içeriğin görünürlüğünü sıfırla
-        contentElement.classList.remove("show");
-        
-        // Yeni içeriği yükle
-        contentElement.innerHTML = html;
-        document.title = pageTitle;
-        window.history.pushState({ "html": html, "pageTitle": pageTitle }, pageTitle, urlPath);
-        
-        // Yüklemeden hemen sonra fade-in animasyonunu tetikle
+        // İçerik değişimini ve animasyon başlatmayı geciktirmek için timeout kullanıyoruz
         setTimeout(() => {
-            contentElement.classList.add("show");
-        }, 10); // 10ms gecikme ile animasyonu başlat
+            contentElement.innerHTML = html;
+            document.title = pageTitle;
+            window.history.pushState({ "html": html, "pageTitle": pageTitle }, pageTitle, urlPath);
+
+            // Yeni içerik yerleştirildikten sonra fade-in animasyonu başlatılıyor
+            contentElement.classList.add('show');
+        }, 150);  // Fade-out animasyonu tamamlandıktan sonra içeriği değiştirmek için 150ms bekleniyor
     }
 
     function loadPageBasedOnPath(pathname) {
         if (routes[pathname]) {
             fetch(routes[pathname].contentPath)
-                .then(response => response.text())
+                .then(response => {
+                    if (!response.ok) {
+                        throw new Error('Network response was not ok');
+                    }
+                    return response.text();
+                })
                 .then(html => {
-                    updatePageContent(html, routes[pathname].title, pathname);
-                });
+                    contentElement.classList.remove('fade-in');  // Animasyonu sıfırla
+                    contentElement.style.opacity = 0;  // Sayfa yüklendiğinde transparan yap
+  
+                    setTimeout(() => {
+                        contentElement.innerHTML = html;
+                        document.title = routes[pathname].title;
+                        window.history.pushState(null, "", pathname); // URL'yi güncelle
+                        setActiveTabFromURL(); // Tabbar'ı güncelle
+  
+                        // Animasyonu başlat
+                        contentElement.classList.add('fade-in');
+                        contentElement.style.opacity = 1;  // Sayfa içeriğini yavaşça göster
+                    }, 100);
+                })
+                .catch(error => console.error('There has been a problem with your fetch operation:', error));
         } else {
-            console.error("404: Page not found");
+            console.error("404: Page not found for", pathname);
         }
     }
 
-    document.querySelectorAll("[data-url]").forEach(button => {
-        button.addEventListener("click", function (event) {
-            event.preventDefault(); 
-            const urlPath = this.getAttribute("data-url");
-            loadPageBasedOnPath(urlPath);
+    function setActiveTabFromURL() {
+        const path = window.location.pathname;
+        const items = document.querySelectorAll('.nav-item, [data-url-path]'); // Tüm elemanları yeniden seç
+        let matchingItem = null;
+
+        items.forEach(item => {
+            const targetPath = item.getAttribute('data-target-path') || item.getAttribute('data-url-path');
+            if (path.startsWith(targetPath)) {
+                matchingItem = item;
+            }
+        });
+
+        if (matchingItem) {
+            handleIndicator(matchingItem);
+        }
+    }
+
+    // MutationObserver ile dinamik olarak eklenen elemanları izleyin
+    const observer = new MutationObserver(() => {
+        const items = document.querySelectorAll('.nav-item, [data-url-path]');
+        items.forEach((item) => {
+            item.removeEventListener('click', handleItemClick); // Önceki dinleyiciyi kaldırın
+            item.addEventListener('click', handleItemClick);
+
+            if (item.classList.contains('is-active')) {
+                handleIndicator(item);
+            }
         });
     });
 
+    observer.observe(document.body, { childList: true, subtree: true });
+
+    function handleItemClick(e) {
+        e.preventDefault();
+        const targetPath = this.getAttribute('data-target-path') || this.getAttribute('data-url-path');
+        if (targetPath) {
+            loadPageBasedOnPath(targetPath);
+            handleIndicator(this);
+        }
+    }
+
+    setActiveTabFromURL();
     loadPageBasedOnPath(window.location.pathname);
 
     window.onpopstate = function (event) {
         if (event.state) {
-            const contentElement = document.getElementById("content");
-            contentElement.classList.remove("show");
-            contentElement.innerHTML = event.state.html;
-            document.title = event.state.pageTitle;
+            // Fade-out başlangıcı
+            contentElement.classList.remove('show');
+
+            // İçerik değişimini ve animasyon başlatmayı geciktirmek için timeout kullanıyoruz
             setTimeout(() => {
-                contentElement.classList.add("show");
-            }, 10);
+                contentElement.innerHTML = event.state.html;
+                document.title = event.state.pageTitle;
+
+                // Yeni içerik yerleştirildikten sonra fade-in animasyonu başlatılıyor
+                contentElement.classList.add('show');
+            }, 150);  // Fade-out animasyonu tamamlandıktan sonra içeriği değiştirmek için 150ms bekleniyor
+        } else {
+            loadPageBasedOnPath(window.location.pathname);
         }
     };
-};
+});
