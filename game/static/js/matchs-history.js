66a261fe5bbf6ef670e12cc460bec3eba6c48254
mucahitcakmak <mucoox18@gmail.com>
2024-08-26 16:07:16 +0300

    match history sayfasının tasarımı iyileştirildi. turnuva tasarımı kaldı.

--- game/static/js/matchs-history.js
+++ game/static/js/matchs-history.js
@@ -1,5 +1,6 @@
 function selectItem(element) {
-    var items = document.getElementsByClassName('history-item');
+    var items = document.getElementsByClassName('match-btn');
+    console.log(items);
     for (var i = 0; i < items.length; i++) {
         items[i].classList.remove('new-cool');
     }
